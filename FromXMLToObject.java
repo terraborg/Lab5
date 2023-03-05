abf66b3a5ba003b7996ac2703dda448de7e70f5a
Волович Алексей Александрович <63094987+terraborg@users.noreply.github.com>
2023-03-05 15:38:55 +0500

    javadoc improved

--- FromXMLToObject.java
+++ FromXMLToObject.java
@@ -1,6 +1,7 @@
 package core.file;
 
 import core.HumanBeing;
+import core.utils.generatorID;
 
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
@@ -10,8 +11,7 @@ import java.util.ArrayList;
 
 /**
  * @author Volovich Alexey
- * Этот класс считывает объекты класса HumanBeing из XML файла.
- * при помощи внутреннего класса InnerXMLReader достигается получение доступа к приватным полям.
+ * @deprecated  Этот класс считывает объекты класса HumanBeing из XML файла. При помощи внутреннего класса InnerXMLReader достигается получение доступа к приватным полям.
  * @see HumanBeing
  */
 public class FromXMLToObject implements FileIn{
@@ -37,13 +37,16 @@ public class FromXMLToObject implements FileIn{
     public HumanBeing[] readFile() throws FileNotFoundException, XMLStreamException {
         XMLStreamReader reader = XMLInputFactory.newInstance().createXMLStreamReader(path, new BufferedReader(new FileReader(path)));
         ArrayList<HumanBeing> res = new ArrayList<>();
+        long mx = 0;
         while (reader.hasNext())
         {
             reader.next();
             if(reader.isStartElement() && reader.hasName() && reader.getLocalName().equals("HumanBeing")) {
                 res.add(new HumanBeing(reader));
+                mx = Math.max(res.get(res.size()-1).getId(),mx);
             }
         }
+        generatorID.setId(mx+1);
         reader.close();
         return res.toArray(new HumanBeing[0]);
     }
